625cfbe6ba5c60965da693cd5a2537475c8c098c
Philipp Kurz <philipp_kurz@berkeley.edu>
2020-04-30 18:27:58 -0700

    Switched back to BFS and improved a bit

--- solver.py
+++ solver.py
@@ -26,34 +26,16 @@ def findSpanningTreeDFS(G):
     n = len(list(G.nodes()))
     visited = [False] * n
     start = random.randrange(n)
-    fringe = []
-
-    fringe.append((-1, start))
+    fringe = [(-1, start)]
     while len(fringe) > 0:
-        # edge = fringe.pop(0)
-        edge = heapq.heappop(fringe)
+        edge = fringe.pop()
         if not visited[edge[1]]:
             visited[edge[1]] = True
             T.add_node(edge[1])
             if edge[0] > -1:
                 weight = G.get_edge_data(edge[0], edge[1])['weight']
                 T.add_edge(edge[0], edge[1], weight=weight)
-
             neighbors = list(nx.neighbors(G, edge[1]))
-
-            # neighbors_inv_weights = []
-            # total = 0
-            # for neighbor in neighbors:
-            #     inv_weight = 1/G.get_edge_data(edge[1], edge[neighbor])['weight']
-            #     neighbors_inv_weights.append((neighbor, inv_weight))
-            #     total += inv_weight
-            # normalized_neighbor_inv_weights = []
-            # for neighbor in neighbors_inv_weights:
-            #     normalized_neighbor_inv_weights.append((neighbor[0], neighbor[1]/total))
-            # normalized_neighbor_inv_weights = sorted(normalized_neighbor_inv_weights, key=lambda x: x[1])
-
-
-
             random.shuffle(neighbors)
             for neighbor in neighbors:
                 if not visited[neighbor]:
@@ -66,8 +48,7 @@ def findSpanningTreeDFS(G):
     n = len(list(G.nodes()))
     visited = [False] * n
     start = random.randrange(n)
-    fringe = []
-    fringe.append((-1, start))
+    fringe = [(-1, start)]
     while len(fringe) > 0:
         edge = fringe.pop()
         if not visited[edge[1]]:
@@ -88,28 +69,27 @@ def findSpanningTreeBFS(G):
     T = nx.Graph()
     n = len(list(G.nodes()))
     visited = [False] * n
-    # start = random.randrange(n)
+    start = random.randrange(n)
 
-    node_degree = Sort_Tuple(G.degree(list(G.nodes())), 1)
-    max_degree_nodes = []
-    max_deg = 0
-    for node in node_degree:
-        max_deg = max(max_deg, node[1])
-    for node in node_degree:
-        if node[1] == max_deg:
-            max_degree_nodes.append(node)
-    max_deg_avg = []
-    if len(max_degree_nodes) > 1:
-        for node in max_degree_nodes:
-            max_deg_avg.append((node[0], AverageEdgeCost(G, node[0])))
-        max_deg_avg = Sort_Tuple(max_deg_avg, 1)
-        start = max_deg_avg.pop(0)[0]
-    else:
-        start = max_degree_nodes.pop()[0]
+    # node_degree = Sort_Tuple(G.degree(list(G.nodes())), 1)
+    # max_degree_nodes = []
+    # max_deg = 0
+    # for node in node_degree:
+    #     max_deg = max(max_deg, node[1])
+    # for node in node_degree:
+    #     if node[1] == max_deg:
+    #         max_degree_nodes.append(node)
+    # max_deg_avg = []
+    # if len(max_degree_nodes) > 1:
+    #     for node in max_degree_nodes:
+    #         max_deg_avg.append((node[0], AverageEdgeCost(G, node[0])))
+    #     max_deg_avg = Sort_Tuple(max_deg_avg, 1)
+    #     start = max_deg_avg.pop(0)[0]
+    # else:
+    #     start = max_degree_nodes.pop()[0]
 
-    fringe = []
+    fringe = [(-1, start)]
 
-    fringe.append((-1, start))
     while len(fringe) > 0:
         edge = fringe.pop(0)
 
@@ -200,11 +180,13 @@ def solve(G):
     leaves = []
     for node in node_degree:
         if node[1] == 1:
-            leaves.append(node[0])
-    random.shuffle(leaves)
+            edge = list(G.edges(node[0]))[0]
+            leaves.append((node[0], G.get_edge_data(edge[0], edge[1])['weight']))
+    # random.shuffle(leaves)
+    leaves = Sort_Tuple(leaves, 1)
     score = average_pairwise_distance(T)
     while len(leaves) > 0:
-        leaf = leaves.pop()
+        leaf = leaves.pop()[0]
         if T.degree(leaf) == 1:
             newT = T.copy()
             newT.remove_node(leaf)
